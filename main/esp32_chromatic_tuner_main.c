0a4b6ad20f63a22860664b8028fac200a0410cf3
Boyd Timothy <btimothy@gmail.com>
2024-07-27 09:43:48 -0600

    Set the ADC input to #4 and change attenuation to max

--- main/esp32_chromatic_tuner_main.c
+++ main/esp32_chromatic_tuner_main.c
@@ -17,7 +17,7 @@
 #define _EXAMPLE_ADC_UNIT_STR(unit)         #unit
 #define EXAMPLE_ADC_UNIT_STR(unit)          _EXAMPLE_ADC_UNIT_STR(unit)
 #define EXAMPLE_ADC_CONV_MODE               ADC_CONV_SINGLE_UNIT_1
-#define EXAMPLE_ADC_ATTEN                   ADC_ATTEN_DB_0
+#define EXAMPLE_ADC_ATTEN                   ADC_ATTEN_DB_12
 #define EXAMPLE_ADC_BIT_WIDTH               SOC_ADC_DIGI_MAX_BITWIDTH
 
 #if CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2
@@ -33,13 +33,15 @@
 #define EXAMPLE_READ_LEN                    256
 
 #if CONFIG_IDF_TARGET_ESP32
-static adc_channel_t channel[2] = {ADC_CHANNEL_6, ADC_CHANNEL_7};
+// static adc_channel_t channel[2] = {ADC_CHANNEL_6, ADC_CHANNEL_7};
+static adc_channel_t channel[1] = {ADC_CHANNEL_4};
 #else
-static adc_channel_t channel[2] = {ADC_CHANNEL_2, ADC_CHANNEL_3};
+// static adc_channel_t channel[2] = {ADC_CHANNEL_2, ADC_CHANNEL_3};
+static adc_channel_t channel[1] = {ADC_CHANNEL_4};
 #endif
 
 static TaskHandle_t s_task_handle;
-static const char *TAG = "EXAMPLE";
+static const char *TAG = "TUNER";
 
 static bool IRAM_ATTR s_conv_done_cb(adc_continuous_handle_t handle, const adc_continuous_evt_data_t *edata, void *user_data)
 {
